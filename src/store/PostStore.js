ac690700d2a33299774f4b7cf7159f53f6ccc719
BillClinton <bclinton@appunto.net>
2023-03-30 13:04:31 -0400

    feat: add a Pinia store w/ fetch capability

---
+++ src/store/PostStore.js
@@ -0,0 +1,48 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const usePostStore = defineStore("post", {
+  state: () => ({
+    data: [],
+    total: 0,
+    host: "https://jsonplaceholder.typicode.com",
+    path: "/posts",
+    limit: 20,
+    start: 0,
+  }),
+  actions: {
+    load() {
+      const me = this;
+
+      let url = `${me.host}${me.path}`;
+
+      if (me.limit > 0) {
+        url += `?_limit=${me.limit}&_start=${me.start}`;
+      }
+
+      return new Promise((resolve, reject) => {
+        axios
+          .get(url)
+          .then((response) => {
+            this.data = response.data;
+            this.total = parseInt(response.headers["x-total-count"]);
+            resolve(response);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    },
+    destroy(a, b, c) {
+      console.log(a);
+      console.log(b);
+      console.log(c);
+    },
+    setLimit: function (limit) {
+      this.limit = limit;
+    },
+    setStart: function (start) {
+      this.start = start;
+    },
+  },
+});
