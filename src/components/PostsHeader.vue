f38a7450d98fda89e9b763b74b6efe9d36ed15b6
BillClinton <bclinton@appunto.net>
2023-03-31 12:26:52 -0400

    feat: add header

---
+++ src/components/PostsHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-col cols="5">
+    <h1>Posts</h1>
+  </v-col>
+
+  <v-col cols="7" class="text-right">
+    <!-- Add Post button -->
+    <v-btn
+      icon="mdi-plus"
+      size="small"
+      color="primary"
+      @click="send({ type: 'ADD' })"
+    />
+  </v-col>
+</template>
+
+<script>
+import { useDataTableMachine } from "@/machines/DataTableMachine.js";
+
+export default {
+  setup() {
+    const { send } = useDataTableMachine();
+
+    return {
+      send,
+    };
+  },
+};
+</script>
