ea6d6ce3f6748dc3582c08a569eebfbd95508db1
BillClinton <bclinton@appunto.net>
2023-04-02 18:03:36 -0400

    refactor: alter Header

--- src/components/PostsHeader.vue
+++ src/components/PostsHeader.vue
@@ -1,11 +1,11 @@
 <template>
   <v-col cols="3">
-    <h1>Posts</h1>
+    <h1>vue-xstate-example</h1>
   </v-col>
 
   <v-col cols="6">
     <span class="text-caption-5"
-      >Using api from
+      >Using "posts" api from
       <a target="_new" href="https://jsonplaceholder.typicode.com"
         >https://jsonplaceholder.typicode.com</a
       ></span
