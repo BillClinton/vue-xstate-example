d9655d66656658ee183f47e71e6f21cc6f4a0bf0
BillClinton <bclinton@appunto.net>
2023-04-02 22:35:17 -0400

    refactor: remove modification unnecessary for demo

--- src/components/PostsDataTable.vue
+++ src/components/PostsDataTable.vue
@@ -42,18 +42,6 @@
         </td>
       </tr>
     </template>
-
-    <template #bottom>
-      <v-data-table-footer
-        :items-per-page-options="[
-          { value: 10, title: '10' },
-          { value: 20, title: '20' },
-          { value: 50, title: '50' },
-          { value: 100, title: '100' },
-        ]"
-      >
-      </v-data-table-footer>
-    </template>
   </v-data-table-server>
 
   <v-dialog v-model="formIsVisible" width="auto">
